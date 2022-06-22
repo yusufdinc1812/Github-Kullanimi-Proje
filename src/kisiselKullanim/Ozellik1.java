4eec1d1ad1d3fe19776c52b11c3ace4ab9d640fc
Yusuf <yusufdinc1812@gmail.com>
2022-06-23 01:38:35 +0300

    yusuf branch 2.degisiklik

--- src/kisiselKullanim/Ozellik1.java
+++ src/kisiselKullanim/Ozellik1.java
@@ -2,6 +2,9 @@ package kisiselKullanim;
 
 public class Ozellik1 {
     public static void main(String[] args) {
+
+
         System.out.println("Yusuf branch'inde calisiyorum");
+        System.out.println("Yusuf branch'inde 2. degisiklik kendime ozel class'da");
     }
 }
